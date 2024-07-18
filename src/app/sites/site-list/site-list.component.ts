305184c8af0233f033fa3dc123f65515b22b206d
zafarsaleem1984 <87990113+zafarsaleem1984@users.noreply.github.com>
2024-07-18 21:28:00 +0500

    Study List and site list api is integrated

--- src/app/sites/site-list/site-list.component.ts
+++ src/app/sites/site-list/site-list.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { ApiService } from '../../api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-site-list',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports:[CommonModule, NgxDatatableModule, HttpClientModule,RouterLink],
+  providers:[ApiService],
   templateUrl: './site-list.component.html',
   styleUrl: './site-list.component.scss',
 })
-export class SiteListComponent {}
+export class SiteListComponent implements OnInit {
+  rows = [];
+  columns = [{ prop: 'name', name: 'Site Name' }, { prop: 'studyShortCode:', name: 'Study short code' }, { prop: 'participantsRecruited:', name: 'Participant Recruited' }];
+  page = {
+    totalElements: 0,
+    pageNumber: 0,
+    size: 1
+  };
+  loadingIndicator = false;
+  constructor(private api: ApiService ) {}
+  ngOnInit(): void {
+    this.fetchSiteList();
+
+  }
+
+  fetchSiteList(){
+    this.loadingIndicator = true;
+    this.api.siteListing({sort: 'id', start:this.page.pageNumber, size:  this.page.size}).subscribe(
+      (res: any) => {
+        this.page.totalElements = res.pagination.totalCount;
+        this.rows = [...res.data];
+        this.loadingIndicator = false;
+      },
+      error => {
+        console.error('Sign in error:', error);
+        this.loadingIndicator = false;
+      }
+    ); 
+  }
+
+  setPage(e: any){
+    this.page.pageNumber = e.offset;
+    this.fetchSiteList();
+  }
+}
--- src/app/studies/study-list/study-list.component.ts
+++ src/app/studies/study-list/study-list.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ApiService } from '../../api.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-study-list',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports:[CommonModule, NgxDatatableModule, HttpClientModule,RouterLink],
+  providers:[ApiService],
   templateUrl: './study-list.component.html',
   styleUrl: './study-list.component.scss',
 })
-export class StudyListComponent {}
+export class StudyListComponent {
+  rows = [];
+  page = {
+    totalElements: 0,
+    pageNumber: 0,
+    size: 1,
+    offset:0
+  };
+  loadingIndicator = false;
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.setPage({ offset: 0 });
+  }
+
+  fetchSiteList() {
+    this.loadingIndicator = true;
+    this.api.studyListing({ sort: 'id', start: this.page.pageNumber, size: this.page.size }).subscribe(
+      (res: any) => {
+
+        this.page.totalElements = res.pagination.totalCount;
+        this.rows = [...res.data];
+        this.loadingIndicator = false;
+      },
+      error => {
+        console.error('Error fetching data:', error);
+        this.loadingIndicator = false;
+      }
+    );
+  }
+
+  setPage(pageInfo: any) {
+    this.page.pageNumber = pageInfo.offset;
+    this.fetchSiteList();
+  }
+}
