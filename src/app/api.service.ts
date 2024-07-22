15bab553c374b55a7caee933ae0ee432fcf115f8
zafarsaleem1984 <87990113+zafarsaleem1984@users.noreply.github.com>
2024-07-22 23:19:10 +0500

    signout

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,53 +2,75 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../environments/environment.development';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   http = inject(HttpClient);
+ 
   constructor() {}
 
   getToken(): string | null {
     // Implement logic to retrieve the token from local storage, session storage, etc.
-    return localStorage.getItem('token');
+    return localStorage.getItem('token') ? localStorage.getItem('token'): null;
   }
 
   signIn(email: string, password: string): Observable<any> {
+   const  headers: any = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`
+    });
     const signInUrl = `${environment.apiUrl}auth/sign-in`;
     const payload = { email, password };
     return this.http.post(signInUrl, payload);
   }
 
   signOut(): Observable<any> {
+    const  headers: any = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`
+    });
     const signInUrl = `${environment.apiUrl}auth/sign-out`;
-    return this.http.post(signInUrl, {});
+    return this.http.post(signInUrl, {headers});
   }
 
   siteListing(params: any): Observable<any> {
+    const  headers: any = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`
+    });
     const signInUrl = `${environment.apiUrl}sites`;
-    return this.http.get(signInUrl, { params: params });
+    return this.http.get(signInUrl, { params: params, headers: headers });
   }
 
   studyListing(params: any): Observable<any> {
+    const  headers: any = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`
+    });
     const signInUrl = `${environment.apiUrl}studies`;
-    return this.http.get(signInUrl, { params: params });
+    return this.http.get(signInUrl, { params: params, headers: headers });
   }
 
   measurements(params: any): Observable<any> {
+    const  headers: any = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`
+    });
     const signInUrl = `${environment.apiUrl}lists/measurements`;
-    return this.http.get(signInUrl, { params: params });
+    return this.http.get(signInUrl, { params: params, headers: headers });
   }
 
   getvideodiarytopics(params: any): Observable<any> {
+    const  headers: any = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`
+    });
     const signInUrl = `${environment.apiUrl}lists/video-diary-topics`;
-    return this.http.get(signInUrl, { params: params });
+    return this.http.get(signInUrl, { params: params, headers: headers });
   }
 
   postStudy(information: any): Observable<any> {
+    const  headers: any = new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`
+    });
     const signInUrl = `${environment.apiUrl}studies`;
-    return this.http.post(signInUrl, information);
+    return this.http.post(signInUrl, information, {headers});
   }
 }
