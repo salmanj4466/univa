9563676e83d15ec2bbd6e6afe95d26e306852eaa
zafarsaleem1984 <87990113+zafarsaleem1984@users.noreply.github.com>
2024-07-23 15:47:34 +0500

    prerender services is off

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -4,7 +4,8 @@ import { CanActivateFn, Router } from '@angular/router';
 export const authGuard: CanActivateFn = (route, state) => {
   // return true;
   console.log('ahsan');
-  if (localStorage.getItem('token')) {
+  const info = localStorage.getItem('token');
+  if (info) {
     return true; // User is authenticated, allow access
   } else {
     inject(Router).navigate(['/signin']);
