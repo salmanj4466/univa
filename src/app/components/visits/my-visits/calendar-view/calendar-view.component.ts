8816ab98019f08ceee6c3be49c542407f56babe4
zafarsaleem1984 <87990113+zafarsaleem1984@users.noreply.github.com>
2024-08-14 20:10:37 +0500

    univa.health | set the code

--- src/app/components/visits/my-visits/calendar-view/calendar-view.component.ts
+++ src/app/components/visits/my-visits/calendar-view/calendar-view.component.ts
@@ -5,6 +5,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment.development';
 
 @Component({
   selector: 'app-calendar-view',
@@ -44,7 +45,7 @@ export class CalendarViewComponent implements OnInit {
     const startDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
     const endDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
 
-    this.http.get(`/api/v1/sessions?dateStart=${startDate.toISOString()}&dateEnd=${endDate.toISOString()}`)
+    this.http.get(`${environment.apiUrl}sessions?dateStart=2024-07-01&dateEnd=2024-07-31`)
       .subscribe((response: any) => {
         this.calendarOptions.events = response.map((session: any) => ({
           title: `Session #${session.number} with ${session.participantCode}`,
@@ -93,5 +94,13 @@ export class CalendarViewComponent implements OnInit {
       }, (error: any) => {
         console.error('Failed to load sessions:', error);
       });
+  }
+
+  formatDateToMMDDYYYY(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
   }
 }
